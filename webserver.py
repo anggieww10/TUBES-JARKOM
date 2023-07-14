517ef51cd273e23a982d18c976a75800454a431d
anggieww10 <121475070+anggieww10@users.noreply.github.com>
2023-07-14 22:45:43 +0700

    Add files via upload

---
+++ webserver.py
@@ -0,0 +1,30 @@
+from socket import *
+from os import path
+
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', 8888))
+serverSocket.listen(1)
+while True:
+    print("Server is running")
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        print(filename)
+        f = open(filename[1:])
+        outputdata = f.read()
+        f.close()
+        connectionSocket.send('HTTP/1.0 200 OK\r\n\r\n'.encode())
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.close()
+    except IOError:
+        print("404 Page Not Found")
+        connectionSocket.send('HTTP/1.0 404 Not Found\r\n\r\n'.encode())
+        connectionSocket.send(
+            b'<html><head></head><body><h1>404 Not Found</h1></body></html>')
+        connectionSocket.close()
+
+
+serverSocket.close()
